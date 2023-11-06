2d9e53facc2c136655ecdc35ef18cd28f04f73d5
ilham05 <ilhammaulana05303005@gmail.com>
2023-11-06 09:57:00 +0700

    redeploy

--- webpack.common.js
+++ webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist/public'),
+    path: path.resolve(__dirname, 'public'),
     clean: true,
   },
   module: {
@@ -63,7 +63,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/public'),
+          to: path.resolve(__dirname, 'public'),
         },
       ],
     }),
